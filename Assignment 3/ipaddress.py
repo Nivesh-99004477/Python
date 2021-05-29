b2b09b2fc743c1430223840a9eebf7b69cdf481d
Nivesh-99004477 <84910757+Nivesh-99004477@users.noreply.github.com>
2021-05-29 17:49:47 +0530

    Delete Assignment 3 directory

--- Assignment 3/ipaddress.py
+++
@@ -1,9 +0,0 @@
-def ip(num):
-    parts = num.split('.')
-    x=(int(parts[0]) << 24) + (int(parts[1]) <<16)  + (int(parts[2]) << 8) + int(parts[3])
-    return x
-
-
-
-z=ip('4.5.6.7')
-print(z)
--- Assignment 3/mexican.py
+++
@@ -1,8 +0,0 @@
-s= 'nivesh'
-new=[]
-for i, val in enumerate(s[:]):
-    up=s[i].upper()
-    c=s[:i] + up + s[i+1:]
-    new.append(c)
-
-    print(new)
